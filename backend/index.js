51c095e768e601816704829cd734bf8d8786ec9f
Nitin <ethicalnitinhere@example.com>
2025-02-10 16:41:04 +0530

    lololo

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,18 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 const compression = require('compression');
 require('dotenv').config();
-const bodyParser= require('body-parser');
-const path = require("path"); // <-- Add this
+const bodyParser= require('body-parser')
 const productRoutes = require("./routes/productRoutes");
 const paymentRoutes = require('./routes/paymentRoutes');
-const Product = require("./models/productData");
+const Product = require("./models/productData"); // Import your Mongoose model
+const { urlencoded } = require('body-parser');
 
 const app = express();
 
 const MONGO_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 3040;
 
+
+const PORT = process.env.PORT || 3040;
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'OPTIONS'], credentials: true }));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,19 +25,14 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-// API Routes
+  
+
+    
 app.use('/api/payment', paymentRoutes);
 app.use("/api/products", productRoutes);
 
-// ---------------------------
-// Serve static assets in production
-// ---------------------------
-app.use(express.static(path.join(__dirname, "build")));
 
-// Catch-all route: for any request that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
